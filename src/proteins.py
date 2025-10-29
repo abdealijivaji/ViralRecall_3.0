a6de45bc8465aad35238309bcdddc6077d1b1868
abdealijivaji <abdeali@vt.edu>
2025-10-29 16:10:17 -0400

    implement protein prediction and HMMER search functionality

---
+++ src/proteins.py
@@ -0,0 +1,71 @@
+import pyrodigal_gv
+from pyhmmer import easel, plan7, hmmer
+from collections import defaultdict, namedtuple
+from pyfaidx import Fasta
+
+
+orf_finder = pyrodigal_gv.ViralGeneFinder(meta=True)
+
+def predict_proteins(input : Fasta, contigs : list[str] , outbase: str) -> tuple[list, list]:
+	''' 
+	Predict proteins using pyrodigal-gv and returns namedtuples of proteins and their headers 
+	Also writes predictions in CDS, AA, and gff3 formats.
+	'''
+	prot_out = outbase +  ".faa"
+	cds_out = outbase +  ".cds.fasta"
+	gff_out = outbase + ".gff"
+
+	proteins : list = []
+	header : list[tuple] = []
+	Header = namedtuple("Header", ["contig", "query", "pstart", "pend", "pstrand", "gen_code"])
+	
+	with open(prot_out, "w") as prot, open(cds_out, "w") as cds, open(gff_out, "w") as gff:
+		for seqrecord in contigs:
+			sequence = bytes(str(input[seqrecord]), 'UTF-8') # loading the sequence as bytes
+			genes = orf_finder.find_genes(sequence)
+			genes.write_translations(prot, sequence_id=seqrecord)
+			genes.write_genes(cds, sequence_id=seqrecord)
+			genes.write_gff(gff, sequence_id=seqrecord)
+			for n, gene in enumerate(genes, start= 1):
+				aa = gene.translate()
+				prot_id = seqrecord + "_" + str(n)
+				head = Header(seqrecord, prot_id, gene.begin, gene.end, gene.strand, gene.translation_table)
+				header.append(head)
+				# proteins.append((prot_id, aa))
+				if len(aa) < 100000: # HMMER can't take proteins longer than 100k aa
+					proteins.append(easel.TextSequence(name = bytes(prot_id,  'UTF-8'), sequence= aa).digitize(amino))
+	return proteins, header
+
+amino = easel.Alphabet.amino()
+tbl_head = b"#                                                               --- full sequence ---- --- best 1 domain ---- --- domain number estimation ----\ntarget_name        accession  query_name           accession    E-value  score  bias   E-value  score  bias   exp reg clu  ov env dom rep inc target_description\n"
+
+def search_with_pyhmmer(proteins: list, hmm_path: str, out_base: str, evalue: float) -> list[tuple]:
+	
+	results : list[tuple] = []
+	Result = namedtuple("Result", ["contig", "query", "HMM_hit", "bitscore", "evalue"])	
+	
+	digseqs = easel.DigitalSequenceBlock(amino, proteins)
+	
+	hmmout = out_base + ".tblout"
+	
+	with plan7.HMMFile(hmm_path) as hmm_file:
+		# Convert protein predictions into pyhmmer Sequence objects
+		# for hmm in hmm_file:
+		# 	print(hmm.cutoffs)
+		hits = hmmer.hmmsearch(hmm_file, digseqs, E=evalue)
+		with open(hmmout, 'wb') as outfile:
+			outfile.write(tbl_head)
+			for hitlist in hits:
+				hitlist.write(outfile, header=False) 
+				for hit in hitlist:
+					if hit.included:
+						Contig =  hit.name.decode().rsplit("_", maxsplit=1)[0]
+						eval = "%.3g" % hit.evalue
+						results.append(Result(
+							Contig ,
+							hit.name.decode() ,
+							hitlist.query.name.decode(),
+							round(hit.score, 2),
+							eval
+						))
+	return results
